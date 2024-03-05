277b46c4320d0d350188511d94134de468d2d921
DevilGamerDev <devilgamerdev@gmail.com>
2024-03-04 22:52:32 -0500

    Fixing minor issues #1

--- New_Web/linux-goblins-web/src/pages/BuildPage/index.jsx
+++ New_Web/linux-goblins-web/src/pages/BuildPage/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavBar } from '../../components';
 import "./index.css"
-import "./asset1.css"
+//import "./asset1.css"
 import TextTransition, { presets } from 'react-text-transition';
 
 
